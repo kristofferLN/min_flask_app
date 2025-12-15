127af1100b3d1be58bbdca92a582d7fa68fe8ce8
KristofferLN <kristoffer.LN@hotmail.com>
2025-12-15 15:55:15 +0100

    viser om klokken er før eller efter 16

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    nu = datetime.now()
+    if nu.hour < 16:
+        tidstekst = "Klokken er før 16"
+    else:
+        tidstekst = "Klokken er efter 16"
+    return render_template("index.html", tidstekst=tidstekst)
 
 if __name__ == "__main__":
     app.run(debug=True)
